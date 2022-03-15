64ddd597e4749a55661bdb31303d1ca99d4b797c
Andrei Iacob <77151602+IacobAndrei15@users.noreply.github.com>
2022-03-15 11:41:18 +0200

    compress ratio compare

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ import java.util.Random;
 
 public class Main {
 
-    static int lengthOfPrime = 64;
+    static int lengthOfPrime = 512;
     static int lengthOfRandom = 1_000_000;
     static BigInteger seed;
     static BigInteger n;
@@ -69,6 +69,17 @@ public class Main {
 
         System.out.println("Compressed pseudo-random number length is: " + compressed.size() + " || Original pseudo-random number length is: " + randomNumberBBS.length());
         System.out.println("Compression ration is: " + (double)compressed.size()/lengthOfRandom);
+
+        StringBuilder oneZero = new StringBuilder();
+        for( int index = 0 ; index < 1_000_000 ; index++ )
+            if( index % 2 == 0 )
+                oneZero.append("1");
+        else
+            oneZero.append("0");
+
+        List<Integer> compressedOneZero = LZW.encode(oneZero.toString());
+        System.out.println("Compressed zero-one number length is: " + compressedOneZero.size() + " || Original zero-one number length is: " + oneZero.length());
+        System.out.println("Compression ration is: " + (double)compressedOneZero.size()/lengthOfRandom);
     }
 
     static String jacobiSymbol(BigInteger a, BigInteger n){
@@ -95,7 +106,6 @@ public class Main {
                 break;
             }
 
-            // TO DO - LINE 9 - 11
             if( b.mod( BigInteger.valueOf(4) ).compareTo( BigInteger.valueOf(3) ) == 0 )
                 if( n.mod( BigInteger.valueOf(4) ).compareTo( BigInteger.valueOf(3) ) == 0 )
                     s *= -1;
@@ -115,10 +125,10 @@ public class Main {
         long numberOfOne  = 0;
         long numberOfZero = 0;
 
-        for( long position = 0 ; position < 1000000 ; position++ ){
+        for( long position = 0 ; position < 100 ; position++ ){
 
             String jacobi = jacobiSymbol( seed, n );
-
+            //System.out.println(jacobi);
             if( jacobi.compareTo("1") == 0 ){
                 randomNumberJacobi.append("1");
                 numberOfOne++;
@@ -128,6 +138,7 @@ public class Main {
             }
 
             seed = seed.add(BigInteger.valueOf(1));
+
         }
 
         //System.out.println("Jacobi pseudo-random number is:");
@@ -140,8 +151,9 @@ public class Main {
         //BBS();
         //jacobiPseudoRandom();
 
-        BigInteger nr1 = new BigInteger("27129");
-        BigInteger nr2 = new BigInteger("1424743");
+        BigInteger nr1 = new BigInteger("134256139287544233423179392268687970851788538273761494746768964886465967391092377001067138608285696495046053782388184521874750960269008328068841529295181707698825604871839270829570019710186824976858713250752730471419354137096373503960907452651547963489623759066542040096683459770016349373775733544376491487280");
+        BigInteger nr2 = new BigInteger("134256139287544233423179392268687970851788538273761494746768964886465967391092377001067138608285696495046053782388184521874750960269008328068841529295181707698825604871839270829570019710186824976858713250752730471419354137096373503960907452651547963489623759066542040096683459770016349373775733544376491487281");
+        nr1 = nr1.multiply(nr1);
         System.out.println( jacobiSymbol(nr1, nr2));
     }
 }

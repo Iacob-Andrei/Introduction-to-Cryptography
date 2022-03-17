31ddc0e8e30666ab8f2d3c991a5db709db9cb30e
Andrei Iacob <77151602+IacobAndrei15@users.noreply.github.com>
2022-03-17 13:42:06 +0200

    jacobi symbol fixed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -30,14 +30,30 @@ public class Main {
 
         n = p.multiply(p);
 
-        System.out.println("n = " + p + " * " + q + " = " + n );
+        System.out.println("Value of q is: "  + q );
+        System.out.println("Value of p is: "  + p );
+        System.out.println("Value of n is: "  + n );
 
         Date date = new Date();
         seed = BigInteger.valueOf( date.getTime() );
         seed = seed.multiply(seed);
         seed = seed.mod(n);
 
-        System.out.println("The seed is: " + seed);
+        System.out.println("Value of seed is: " + seed);
+    }
+
+    static void printZeroOneNumberRatio(){
+
+        StringBuilder oneZero = new StringBuilder();
+        for( int index = 0 ; index < 1_000_000 ; index++ )
+            if( index % 2 == 0 )
+                oneZero.append("1");
+            else
+                oneZero.append("0");
+
+        List<Integer> compressedOneZero = LZW.encode(oneZero.toString());
+        System.out.println("Compressed zero-one number length is: " + compressedOneZero.size() + " || Original zero-one number length is: " + oneZero.length());
+        System.out.println("Compression ration is: " + (double)compressedOneZero.size()/lengthOfRandom);
     }
 
     static void BBS(){
@@ -70,85 +86,82 @@ public class Main {
         System.out.println("Compressed pseudo-random number length is: " + compressed.size() + " || Original pseudo-random number length is: " + randomNumberBBS.length());
         System.out.println("Compression ration is: " + (double)compressed.size()/lengthOfRandom);
 
-        StringBuilder oneZero = new StringBuilder();
-        for( int index = 0 ; index < 1_000_000 ; index++ )
-            if( index % 2 == 0 )
-                oneZero.append("1");
-        else
-            oneZero.append("0");
-
-        List<Integer> compressedOneZero = LZW.encode(oneZero.toString());
-        System.out.println("Compressed zero-one number length is: " + compressedOneZero.size() + " || Original zero-one number length is: " + oneZero.length());
-        System.out.println("Compression ration is: " + (double)compressedOneZero.size()/lengthOfRandom);
+        printZeroOneNumberRatio();
     }
 
-    static String jacobiSymbol(BigInteger a, BigInteger n){
+    static BigInteger jacobiSymbol(BigInteger a, BigInteger n) {
 
         BigInteger b = a.mod(n);
         BigInteger c = n;
         int s = 1;
 
-        while( b.compareTo(BigInteger.valueOf(2)) >= 0 ){
+        while (b.compareTo(BigInteger.TWO) >= 0) {
 
-            while( b.mod( BigInteger.valueOf(4) ).compareTo( BigInteger.valueOf(0) ) == 0 ){
-                b = b.divide( BigInteger.valueOf(4) );
+            while(b.mod(BigInteger.valueOf(4)).compareTo(BigInteger.ZERO) == 0) {
+                b = b.divide(BigInteger.valueOf(4));
             }
 
-            if( b.mod( BigInteger.valueOf(2) ).compareTo( BigInteger.valueOf(0) ) == 0 ){
+            if(b.mod(BigInteger.TWO).compareTo(BigInteger.ZERO) == 0) {
 
-                BigInteger cMod8 = c.mod( BigInteger.valueOf(8) );
-
-                if( cMod8.compareTo( BigInteger.valueOf(3) ) == 0 || cMod8.compareTo( BigInteger.valueOf(5) ) == 0) {
+                BigInteger cMod8 = c.mod(BigInteger.valueOf(8));
+                if(cMod8.compareTo(BigInteger.valueOf(3)) == 0 || cMod8.compareTo(BigInteger.valueOf(5)) == 0) {
                     s = -s;
+                    b = b.divide(BigInteger.TWO);
                 }
-                b = b.divide( BigInteger.valueOf(2) );
             }
-
-            if( b.compareTo( BigInteger.valueOf(1) ) == 0 )
+            
+            if(b.compareTo(BigInteger.ONE) == 0) 
                 break;
-
-            BigInteger bMod4 = b.mod( BigInteger.valueOf(4) );
-            BigInteger cMod4 = c.mod( BigInteger.valueOf(4) );
-
-            if( bMod4.compareTo( BigInteger.valueOf(3) ) == 0 &&  cMod4.compareTo( BigInteger.valueOf(3) ) == 0 ){
+            
+            BigInteger bMod4 = b.mod(BigInteger.valueOf(4));
+            BigInteger cMod4 = c.mod(BigInteger.valueOf(4));
+            
+            if(bMod4.compareTo(BigInteger.valueOf(3)) == 0 && cMod4.compareTo(BigInteger.valueOf(3)) == 0) {
                 s = -s;
             }
-
-            BigInteger copyOfB = b;
+            
+            BigInteger oldB = b;
             b = c.mod(b);
-            c = copyOfB;
+            c = oldB;
         }
-
-        b = b.multiply( BigInteger.valueOf(s) );
-        return b.toString();
+        
+        return b.multiply(BigInteger.valueOf(s));
     }
 
-    static void jacobiPseudoRandom(){
+    static void jacobiPseudoRandom() {
 
-        System.out.println("Jacobi pseudo-random number is:");
+        System.out.println("\n\nJacobi Pseudo Random number is:");
 
         randomNumberJacobi = new StringBuilder();
-        long numberOfOne  = 0;
-        long numberOfZero = 0;
+        int numberOfZero = 0;
+        int numberOfOne = 0;
+        BigInteger copyOfSeed = seed;
 
-        for( long position = 0 ; position < 1_000_000 ; position++ ){
+        for(int i = 0; i < lengthOfRandom; i++) {
 
-            String jacobi = jacobiSymbol( seed, n );
+            BigInteger jacobiSymbol = jacobiSymbol(copyOfSeed,n);
 
-            if( jacobi.compareTo("1") == 0 ){
-                randomNumberJacobi.append("1");
+            if( jacobiSymbol.compareTo(BigInteger.ONE) == 0 ){
                 numberOfOne++;
+                randomNumberJacobi.append("1");
             }else{
-                randomNumberJacobi.append("0");
                 numberOfZero++;
+                randomNumberJacobi.append("0");
             }
 
-            seed = seed.add(BigInteger.valueOf(1));
+            copyOfSeed = copyOfSeed.add(BigInteger.ONE);
         }
 
-        System.out.println(randomNumberJacobi);
-        System.out.println();
-        System.out.println("\nNumber of 0: " + numberOfZero + " | Number of 1: " + numberOfOne);
+        System.out.println(randomNumberJacobi.toString());
+
+        System.out.println("\nNumber of 0:" + numberOfZero + " | Number of 1:" + numberOfOne);
+
+        List<Integer> compressed = LZW.encode(randomNumberJacobi.toString());
+
+        System.out.println("Compressed pseudo-random number length is: " + compressed.size() + " || Original pseudo-random number length is: " + randomNumberJacobi.length());
+        System.out.println("Compression ration is: " + (double)compressed.size()/lengthOfRandom);
+
+        printZeroOneNumberRatio();
     }
 
     public static void main(String[] args) {
@@ -156,12 +169,5 @@ public class Main {
         setup();
         //BBS();
         jacobiPseudoRandom();
-
-        /*
-        BigInteger nr1 = new BigInteger("452342");
-        BigInteger nr2 = new BigInteger("5423531");
-        System.out.println( jacobiSymbol(nr1, nr2));
-         */
-
     }
 }
